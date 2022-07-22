619d41024cbee76c511fa22832c748488e301d93
Ismail Mardin <ikmardin88@gmail.com>
2022-07-22 22:20:48 +0200

    Add existing file

---
+++ brain_data/fourier_tranform.py
@@ -0,0 +1,14 @@
+import boto3
+import pandas as pd
+import math
+import io
+
+client = boto3.client('s3')
+
+response = client.get_object(Bucket="sona-brain-data", Key="shubhJul-17-2022.csv")
+
+df = pd.read_csv(response["Body"])
+df_list = list(df.columns.values)
+print(len(df_list))
+
+#https://realpython.com/python-scipy-fft/ - explains how to use scipy with example
