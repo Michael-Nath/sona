035af3f1f7d5351b03833f3f54d57675d99e74f3
Michael <michael.d.nath@gmail.com>
2022-08-10 19:13:59 -0700

    updated plotting function

--- src/analysis/utils/plotting_tools.py
+++ src/analysis/utils/plotting_tools.py
@@ -3,15 +3,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-HEADERS = ['Package Num Channel', 'EXG Channel', 'EXG Channel', 'EXG Channel', 'EXG Channel', 'EXG Channel', 'EXG Channel', 'EXG Channel', 'EXG Channel', "Accel", "Accel", "Accel", "Other", "Other", "Other", "Other", "Other", "Other", "Other", "Analog", "Analog", "Analog", "Timestamp", "Marker"]
-
 def graph_raw_cyton_data(cyton_data: pd.DataFrame):    
     num_cols = len(cyton_data.columns) 
-    print(num_cols)
-    fig, ax = plt.subplots(num_cols)
     for i in range(num_cols):
-        ax[i].plot(cyton_data.iloc[:, i])
-    plt.show()
+        # for each plot, let's title it the header that it comes from
+        plt.title(cyton_data.columns[i])
+        plt.plot(cyton_data.iloc[:, i])
+        plt.show()
 
 
 
