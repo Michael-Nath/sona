6658a156837a34a734aa8d38ced0711042926516
Michael <michael.d.nath@gmail.com>
2022-11-05 20:36:25 -0700

    created system for recording and storing emg data; working on correcting fft

--- .ipynb_checkpoints/cyton_driver-checkpoint.py
+++ .ipynb_checkpoints/cyton_driver-checkpoint.py
@@ -11,7 +11,7 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 
 
-DURATION = 15 # seconds to record data for
+DURATION = 30 # seconds to record data for
 
 def config_board():
 
@@ -59,61 +59,37 @@ def stream(board, args, duration):
     board.release_session()
 
     emg_channels = BoardShim.get_emg_channels(BoardIds.CYTON_BOARD.value)
-    print(emg_channels)
     df = pd.DataFrame(np.transpose(data))
     
     plt.figure()
+    df[emg_channels].plot(subplots=True)
+    plt.savefig('before_processing.png')
     print('Data From the Board')
-    pd.set_option('display.max_columns', None)
-    df.plot(subplots=True)
-    plt.savefig(f'before_processing-test.png')
     print(df.head(10))
+    
+    
+
     return data
-
-
-def stream_and_get_emg_time_only(board, args, duration, fname):
-    board.start_stream(45000, args.streamer_params)
-    time.sleep(duration)
-    data = board.get_board_data()
-    board.stop_stream()
-    emg_channels = BoardShim.get_emg_channels(BoardIds.CYTON_BOARD.value)
-    timestamp_channel = BoardShim.get_timestamp_channel(BoardIds.CYTON_BOARD.value)
-    transformed_data = data.T
-    transformed_data = transformed_data[:, emg_channels + [timestamp_channel]]
-    columns = [f"EMG Channel {i}" for i in range(1, 9)] + ["Timestamp Channel"]
-    transformed_data = pd.DataFrame(transformed_data, columns=columns)
-    transformed_data.to_csv(fname, columns = columns)
-    
-    
-
-
-def run_trials(folder_name, num_trials=10):
-    board, args = config_board()
-    for i in range(1, num_trials + 1):
-        print(f"Starting Trial {i}!")
-        fname = folder_name
-        fname += f"/TRIAL_{i}"
-        fname += date.today().strftime("%b-%d-%Y")
-        stream_and_get_emg_time_only(board, args, DURATION, fname)
-        if i < num_trials:
-            print(f"Finished trial {i}, get ready to start next trial in 3 seconds...")
-            time.sleep(3)
-    board.release_all_sessions()
-            
-            
-
-
 
 def save_data(data):
     date_abbrev = date.today().strftime("%b-%d-%Y")
-    np.savetxt(f'{DURATION}-tested' + date_abbrev + '.csv', data)
+    np.savetxt('tested' + date_abbrev + '.csv', data)
+
     return 'tested' + date_abbrev + '.csv'
 
     
     
 def main():
-    folder_name = "recordings"
-    run_trials(folder_name)
+    print(time.time)
+
+    board, args = config_board()
+    print(time.time)
+
+    data = stream(board, args, DURATION)
+    print(time.time)
+    filename = save_data(data)
+    print(board.get_emg_channels(board.board_id))
+    print(time.time)
 
 if __name__ == "__main__":
     main()
